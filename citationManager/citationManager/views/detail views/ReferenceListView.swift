a6ed4264c02597c3ac2ea5a14b8747bacf461d17
Jort Box <jboxelaar98@outlook.com>
2024-12-09 15:05:20 +0100

    inspector view update

---
+++ citationManager/citationManager/views/detail views/ReferenceListView.swift
@@ -0,0 +1,18 @@
+//
+//  ReferenceListView.swift
+//  Citer
+//
+//  Created by Jort Boxelaar on 09/12/2024.
+//
+
+import SwiftUI
+
+struct ReferenceListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ReferenceListView()
+}
